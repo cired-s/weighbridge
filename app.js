2a1186e79675a8ab1632cd2fb3ba29967791c49e
cired-s <cirednus@gmail.com>
2024-10-04 15:33:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -127,18 +127,19 @@ function applyFilter() {
                 const marker_aIcon = item.檢查合格與否 === 'N' ? redIcon : greenIcon;
                 const marker = L.marker([item.latitude, item.longitude], { icon: marker_aIcon }).addTo(weighbridgeLayer);
                 marker.bindPopup(`
-                <h2>固定地磅</h2>
-                <b>${item.店名 || '無'}</b><br>
-                廠牌: ${item.廠牌 || '無'}<br>
-                型式: ${item.型式 || '無'}<br>
-                器號: ${item.器號 || '無'}<br>
-                Max (kg): ${item.Max_kg || '無'}<br>
-                e (g): ${item.e_g || '無'}<br>
-                檢定日期: ${item.檢定日期 || '無'}<br>
-                檢定合格單號: ${item.檢定合格單號 || '無'}<br>
-                檢查日期: ${item.檢查日期 || '無'}<br>
-                檢查合格單號: ${item.檢查合格單號 || '無'}<br>
-                檢查合格與否: ${item.檢查合格與否 || '無'}
+                <h2>固定地秤</h2>  <!-- 添加"固定地秤"標題 -->
+                <b>${item.所有人}</b><br>
+                地址: ${item.地址}<br>
+                廠牌: ${item.廠牌}<br>
+                型號: ${item.型號}<br>
+                器號: ${item.器號}<br>
+                Max (t): ${item.Max_t}<br>
+                e (kg): ${item.e_kg}<br>
+                檢定合格期限: ${item.檢定合格期限}<br>
+                檢定合格單號: ${item.檢定合格單號}<br>
+                檢查日期: ${item.檢查日期}<br>
+                檢查合格單號: ${item.檢查合格單號}<br>
+                檢查合格與否: ${item.檢查合格與否}
                 `);
                 weighbridgeCount++;
             }
